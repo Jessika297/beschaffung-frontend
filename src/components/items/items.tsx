6bd349c71a9d3a6cab42d11f3614c766a773f7cc
Jess <jessika.29.7.99@hotmail.de>
2023-07-04 11:04:58 +0200

    pitch

--- src/components/items/items.tsx
+++ src/components/items/items.tsx
@@ -1,110 +1,118 @@
-import {items} from "@/util/mockedData";
-import Countdown from "react-countdown";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faFan, faHardDrive, faMicrochip} from "@fortawesome/free-solid-svg-icons";
-import {differenceInDays, isBefore} from "date-fns";
 import React, {useState} from "react";
-import Link from "next/link";
-import {ItemInfo} from "@/util/itemType";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+    faBackward,
+    faComputerMouse,
+    faDownload,
+    faHandHoldingHand,
+    faHandHoldingMedical,
+    faRecycle
+} from "@fortawesome/free-solid-svg-icons";
 
-interface ItemsProps {
-    homepage: boolean;
-}
+const services = [
+    {
+        image: "/recycling.jpg",
+        title: "Recycling",
+        icon: <FontAwesomeIcon icon={faRecycle}/>,
+        textFirst: "Partner",
+        textSecond: "Our Partner will recycle your old harware",
+        expanded: "Recycling is great",
+        index: 0,
+    },
+    {
+        image: "/maintenance.jpg",
+        title: "Maintenance",
+        icon: <FontAwesomeIcon icon={faHandHoldingMedical}/>,
+        textFirst: "3",
+        textSecond: "We maintain our sold Hardware",
+        expanded: "Maintenance is great",
+        index: 1,
+    },
+    {
+        image: "/peripherals.jpg",
+        title: "Peripherals",
+        icon: <FontAwesomeIcon icon={faComputerMouse}/>,
+        textFirst: "Equipment",
+        textSecond: "You can get peripherals from us",
+        expanded: "Peripherals are great",
+        index: 2,
+    },
+    {
+        image: "/dataRecovery.jpg",
+        title: "Data Recovery",
+        icon: <FontAwesomeIcon icon={faBackward}/>,
+        textFirst: "Partner",
+        textSecond: "Our Partner will recover your data",
+        expanded: "Recovery is great",
+        index: 3,
+    },
+    {
+        image: "/installation.jpg",
+        title: "Installation",
+        icon: <FontAwesomeIcon icon={faDownload}/>,
+        textFirst: "Software",
+        textSecond: "You can book Software installation",
+        expanded: "Installation is great",
+        index: 4,
+    },
+    {
+        image: "/warranty.jpg",
+        title: "Warranty",
+        icon: <FontAwesomeIcon icon={faHandHoldingHand}/>,
+        textFirst: "Duration",
+        textSecond: "We offer extended warranty",
+        expanded: "Warranty is great",
+        index: 5,
+    },
+];
 
-const Items = ({homepage}: ItemsProps) => {
-    const [tileMax, setTileMax] = useState(homepage ? 6 : 3);
-    let tileCounter = 0;
+const Services = () => {
+    const [tileMax, setTileMax] = useState(6);
 
     return (
         <ul className="p-1.5 flex flex-wrap">
-            {items.map((item: ItemInfo) => {
-                const daysUntilSale = item.saleUntil && differenceInDays(new Date(item.saleUntil), new Date());
-                const hasSaleDays = daysUntilSale && daysUntilSale > 1;
-                const isSaleUP = item.saleUntil && isBefore(new Date(), item.saleUntil);
-                const listItem = item.id == '1' ? '' : '/#list' + String(Number(item.id) - 1)
-
-                if (item.salePrice && isSaleUP && tileCounter < tileMax) {
-                    tileCounter++;
-
+            {services.map((service) => {
+                if (service.index < tileMax) {
                     return (
-                        <li className="w-full lg:w-1/2 xl:w-1/3 p-1.5" key={item.id}>
-                            <div
-                                className="block bg-zinc-800 rounded-md w-full overflow-hidden pb-4 shadow-lg"
-                            >
+                        <li className="w-full lg:w-1/2 xl:w-1/3 p-1.5" key={service.index}>
+                            <div className="block bg-zinc-800 rounded-md w-full overflow-hidden pb-4 shadow-lg">
                                 <div
                                     className="w-full h-40 bg-center bg-cover relative"
-                                    style={{backgroundImage: `url(/sale.jpg)`}}
+                                    style={{backgroundImage: `url(${service.image})`}}
                                 >
                                     <div
                                         className="absolute grid grid-cols-2 left-1/2 -translate-x-1/2 bottom-2  w-5/6 bg-white rounded-md items-center bg-opacity-30 backdrop-blur-md">
-                                                                        <span
-                                                                            className={'col-span-2 font-extrabold p-2 text-zinc-700 bg-white rounded bg-opacity-60'}>{item.name}</span>
-                                        <div className="p-3">
-                                            <h3 className="font-bold text-red-700">Price</h3>
-                                            <span className="text-red-700 font-semibold">{item.salePrice} €</span>
-                                            <span className="ml-2 line-through decoration-1">{item.price}</span>
+                                        <span
+                                            className={'col-span-2 font-extrabold p-2 text-zinc-700 bg-white rounded bg-opacity-60'}>{service.title}</span>
+                                        <div className="p-3 col-span-2">
+                                            <h3 className="font-bold text-red-700">Click below for more Information</h3>
                                         </div>
                                         <div className="p-3">
-                                            <h3 className="font-semibold">Ending in</h3>
-                                            {hasSaleDays
-                                                ? (<div>{daysUntilSale}+ Days</div>)
-                                                : (item.saleUntil && <Countdown
-                                                    date={item.saleUntil}
-                                                    renderer={({days, hours, minutes}) => (
-                                                        <div>{`${days * 24 + hours}h: ${minutes}m`}</div>
-                                                    )}
-                                                />)}
+                                            <div
+                                                className={'text-center font-bold py-1 bg-gradient-to-tr from-fuchsia-600 to-violet-600 rounded-md w-20'}>MORE
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <h3 className="font-semibold text-lg px-3 mt-2">{item.name}</h3>
-                                <div className="items-center px-3 mt-2 grid grid-cols-2">
+                                <div className={'font-semibold text-lg px-3 mt-2'}>
+                                    {service.icon}
+                                    <span className="ml-2">{service.title}</span>
+                                </div>
+                                <div className="items-center px-3 mt-2">
                                     <div>
-                                        <FontAwesomeIcon icon={faMicrochip}/>
-                                        <span className=" ml-2 text-zinc-400">{item.cpu.name}</span>
+                                        <span className=" ml-2 text-zinc-400">{service.textSecond}</span>
                                     </div>
-                                    <div>
-                                        <FontAwesomeIcon icon={faHardDrive}/>
-                                        <span className=" ml-2 text-zinc-400" title={item.storage.size}>
-                                                      {item.storage.size} GB
-                                                    </span>
-                                    </div>
-                                    <div>
-                                        <FontAwesomeIcon icon={faFan}/>
-                                        <span className=" ml-2 text-zinc-400" title={item.storage.name}>
-                                                      {item.gpu.name}
-                                                    </span>
-                                    </div>
-                                    </div>
-                                    <div className="flex mt-2">
-                                        <div className="p-3 w-1/2">
-                                            <button
-                                                className="bg-gradient-to-tr from-fuchsia-600 to-violet-600  w-full h-12 rounded-md font-semibold"
-                                                onClick={() => console.log('added ' + item.name + ' to cart')}>
-                                                Add to cart
-                                            </button>
-                                        </div>
-                                        <div className="p-3 w-1/2">
-                                            <button
-                                                className="bg-gradient-to-tr from-fuchsia-600 to-violet-600  w-full rounded-md font-semibold h-12 p-px">
-                                                <Link href={`/market` + listItem}
-                                                      className="bg-zinc-800 w-full h-full rounded-md grid place-items-center">
-                                                    View Specs
-                                                </Link>
-                                            </button>
-                                        </div>
-                                    </div>
+                                </div>
                             </div>
-                            </li>
-                        );
-                    } else {
-                        return null;
-                    }
+                        </li>
+                    );
+                } else {
+                    return null;
                 }
-            )}
-            {!homepage && <button onClick={() => setTileMax(tileMax + 20)}>More</button>}
+            })}
+            <button onClick={() => setTileMax(tileMax + 3)}>More</button>
         </ul>
     );
 };
 
-export default Items;
+export default Services;
--- src/components/shopItems/shopItems.tsx
+++ src/components/shopItems/shopItems.tsx
@@ -1,16 +1,12 @@
 import {items} from "@/util/mockedData";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCartArrowDown, faChevronDown, faFan, faHardDrive, faMicrochip,} from "@fortawesome/free-solid-svg-icons";
-import {differenceInDays, isBefore} from "date-fns";
-import Countdown from "react-countdown";
+import {faChevronDown, faFan, faHardDrive, faMicrochip,} from "@fortawesome/free-solid-svg-icons";
 import React, {useCallback, useMemo, useState} from "react";
-import Link from "next/link";
 import {ItemInfo} from "@/util/itemType";
 
 const ShopItems = React.memo(() => {
     const [tileMax, setTileMax] = useState(20);
     const [expandedItems, setExpandedItems] = useState([""]);
-    const [quantity, setQuantity] = useState(1);
     let tileCounter: number = 0;
 
     const handleExpandItem = useCallback((itemId: string) => {
@@ -22,23 +18,6 @@ const ShopItems = React.memo(() => {
             }
         });
     }, []);
-
-    const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = parseInt(e.target.value, 10);
-        if (!isNaN(value)) {
-            setQuantity(value);
-        }
-    };
-
-    const handleInputClick = (e: React.MouseEvent<HTMLInputElement>) => {
-        e.stopPropagation();
-    };
-
-    const handleAddClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.stopPropagation();
-        console.log(`Add clicked. Quantity: ${quantity}`);
-    };
-
 
     const renderExpandedInfo = useCallback((item: ItemInfo) => {
         return (
@@ -83,24 +62,7 @@ const ShopItems = React.memo(() => {
         return (
             <div className={"pt-1"}>
                 <div className={"col-start-2"}>
-                    <div className="px-3 py-2">
-                        <input
-                            type="number"
-                            min={1}
-                            value={quantity}
-                            onChange={handleQuantityChange}
-                            onClick={handleInputClick}
-                            className="w-full h-8 rounded-md bg-zinc-900 text-center text-white"
-                        />
-                    </div>
-                    <div className="px-3 py-2">
-                        <button
-                            className="bg-gradient-to-tr from-fuchsia-600 to-violet-600 w-full h-8 rounded-md font-semibold"
-                            onClick={handleAddClick}
-                        >
-                            Add <FontAwesomeIcon icon={faCartArrowDown}/>
-                        </button>
-                    </div>
+
                     <div className="px-3 py-2">
                         <div
                             className="text-center bg-gradient-to-tr from-fuchsia-600 to-violet-600 w-full rounded-md font-semibold h-8 p-px"
@@ -114,7 +76,7 @@ const ShopItems = React.memo(() => {
                 </div>
             </div>
         );
-    }, [quantity]);
+    }, []);
 
     const renderNormalInfo = useCallback((item: ItemInfo) => {
         const {
@@ -127,60 +89,31 @@ const ShopItems = React.memo(() => {
             name,
         } = item;
 
-        const daysUntilSale =
-            saleUntil && differenceInDays(new Date(saleUntil), new Date());
-        const hasSaleDays = daysUntilSale && daysUntilSale > 1;
-        const isSaleUP = saleUntil && isBefore(new Date(), saleUntil);
-
         return (
-            <div className="col-span-3 p-3 grid grid-cols-3">
-                <h3 className="font-extrabold text-lg mb-2 col-span-3">{name}</h3>
-                <div className="mb-2 p-2">
-                    <div>
-                        <FontAwesomeIcon icon={faMicrochip}/>
-                        <span className="ml-2 text-zinc-400">{gpu.name}</span>
-                    </div>
-                    <div>
-                        <FontAwesomeIcon icon={faHardDrive}/>
-                        <span className="ml-2 text-zinc-400" title={storage.name}>
+            <div className="col-span-3 p-3 grid">
+                <h3 className="font-extrabold text-lg mb-2">{name}</h3>
+                <div>
+                    <FontAwesomeIcon icon={faMicrochip}/>
+                    <span className="ml-2 text-zinc-400">{gpu.name}</span>
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faHardDrive}/>
+                    <span className="ml-2 text-zinc-400" title={storage.name}>
               {storage.size} GB
             </span>
-                    </div>
-                    <div>
-                        <FontAwesomeIcon icon={faFan}/>
-                        <span className="ml-2 text-zinc-400" title={storage.description}>
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faFan}/>
+                    <span className="ml-2 text-zinc-400" title={storage.description}>
               {gpu.name}
             </span>
-                    </div>
                 </div>
-                <div className="mb-2 p-2">
-                    <h3 className="font-semibold">Price</h3>
-                    <span
-                        className={`decoration-1 ${isSaleUP ? "line-through" : ""}`}
-                    >
+                <h3 className="font-semibold">Price</h3>
+                <span
+                    className={`decoration-1`}
+                >
             {price} €
           </span>
-                </div>
-                {isSaleUP && (
-                    <div className="mb-2 p-2">
-                        <h2 className="font-bold text-transparent bg-clip-text bg-gradient-to-br from-purple-400 to-purple-800">
-                            SALE
-                        </h2>
-                        <span className="text-red-700 font-extrabold">
-              {salePrice} €
-            </span>
-                        {hasSaleDays ? (
-                            <div>{daysUntilSale}+ Days</div>
-                        ) : (
-                            <Countdown
-                                date={saleUntil}
-                                renderer={({days, hours, minutes}) => (
-                                    <div>{`${days * 24 + hours}h: ${minutes}m`}</div>
-                                )}
-                            />
-                        )}
-                    </div>
-                )}
             </div>
         );
     }, []);
@@ -188,9 +121,6 @@ const ShopItems = React.memo(() => {
     const renderedItems = useMemo(() => {
         return items.map((item: ItemInfo) => {
             const isExpanded = expandedItems.includes(item.id);
-            const listItem = item.id == '1' ? '' : '/#list' + String(Number(item.id) - 1)
-            const listItemID = item.id == '1' ? '0' : String(Number(item.id) - 1)
-            const isAboveExpanded = expandedItems.includes(listItemID);
 
             if (tileCounter < tileMax) {
                 tileCounter++;
@@ -199,7 +129,7 @@ const ShopItems = React.memo(() => {
                     <li className="w-full p-1 py-2" key={item.id}>
                         <div className="relative">
                             <div
-                                className="text-xl font-extrabold absolute inset-0 flex items-center justify-center text-white bg-red-950 bg-opacity-60  w-11/12"
+                                className="text-xl font-extrabold absolute inset-0 flex items-center justify-center text-white bg-red-950 bg-opacity-60"
                                 style={{
                                     zIndex: 1,
                                     top: 0,
@@ -210,9 +140,9 @@ const ShopItems = React.memo(() => {
                 Not available
               </span>
                             </div>
-                            <Link href={`/market` + listItem}
-                                  className={`cursor-pointer bg-zinc-800 rounded-md overflow-hidden p-1 shadow-lg grid grid-cols-5 w-11/12`}
-                                  onClick={() => handleExpandItem(item.id)}
+                            <div
+                                className={`cursor-pointer bg-zinc-800 rounded-md overflow-hidden p-1 shadow-lg grid grid-cols-2`}
+                                onClick={() => handleExpandItem(item.id)}
                             >
                                 <div
                                     className="w-40 h-40 bg-left bg-cover bg-no-repeat relative border-2 border-zinc-500 rounded-md">
@@ -223,10 +153,15 @@ const ShopItems = React.memo(() => {
                                         style={{objectFit: "contain", backgroundColor: "white"}}
                                     />
                                 </div>
-                                {renderNormalInfo(item)}
-                                {renderButton(item.id, isExpanded)}
-                                {isExpanded && renderExpandedInfo(item)}
-                            </Link>
+                                <div>
+                                    {renderNormalInfo(item)}
+                                    {renderButton(item.id, isExpanded)}
+                                </div>
+                                <div className={'col-span-2'}>
+                                    {isExpanded && renderExpandedInfo(item)}
+                                </div>
+
+                            </div>
                         </div>
                     </li>
                 );
@@ -246,7 +181,7 @@ const ShopItems = React.memo(() => {
 
     return (
         <ul className="p-1.5">
-            <div className="grid grid-cols-1">{renderedItems}</div>
+            <div className="grid grid-cols-2">{renderedItems}</div>
             <button onClick={() => setTileMax((prevTileMax) => prevTileMax + 20)}>
                 More
             </button>
